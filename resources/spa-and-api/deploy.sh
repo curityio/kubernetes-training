842cb00a9daa3c50a50d1ed8dc64475dd21ac1cf
gary.archer <gary.archer@curity.io>
2025-01-30 20:04:24 +0100

    Added routes for application components

--- resources/spa-and-api/deploy.sh
+++ resources/spa-and-api/deploy.sh
@@ -26,9 +26,45 @@ if [ $? -ne 0 ]; then
 fi
 
 #
-# Use routes to expose endpoints from the cluster
+# Get the password protected cookie encryption key details
 #
-kubectl -n applications apply -f gateway-routes.yaml
+TH_COOKIE_KEY_PASS='Password1'
+TH_COOKIE_KEY="$(awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' ../curity/tokenhandler-final/cookie-keys/private-key.pkcs8)"
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+#
+# Dynamically create the file for the OAuth Proxy plugin
+#
+cat << EOF > routes/oauth-proxy-plugin.yaml
+kind: KongPlugin
+apiVersion: configuration.konghq.com/v1
+metadata:
+  name: oauth-proxy
+plugin: oauth-proxy
+config:
+  cookie_key: "$TH_COOKIE_KEY"
+  cookie_key_pass: $TH_COOKIE_KEY_PASS
+EOF
+
+#
+# Apply plugins and then routes for the applications namespace
+#
+kubectl -n applications apply -f routes/phantom-token-plugin.yaml
+kubectl -n applications apply -f routes/cors-plugin.yaml
+kubectl -n applications apply -f routes/oauth-proxy-plugin.yaml
+kubectl -n applications apply -f routes/application-routes.yaml
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+#
+# Add routes to expose endpoints from the cluster and to apply plugins
+#
+kubectl -n curity apply -f routes/cors-plugin.yaml
+kubectl -n curity apply -f routes/oauth-proxy-plugin.yaml
+kubectl -n curity apply -f routes/userinfo-route.yaml
 if [ $? -ne 0 ]; then
   exit 1
 fi
