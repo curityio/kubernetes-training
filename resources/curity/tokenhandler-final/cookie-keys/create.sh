55e809d23ed1d3222fd4a95c51e33917a478241e
gary.archer <gary.archer@curity.io>
2025-01-31 09:55:49 +0100

    Plugin minor fixes

--- resources/curity/tokenhandler-final/cookie-keys/create.sh
+++ resources/curity/tokenhandler-final/cookie-keys/create.sh
@@ -3,16 +3,9 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 #
-# Get the password that protects the private key
-#
-if [ "$TH_COOKIE_KEY_PASS" != '' ]; then
-  echo 'Supply the token handler cookie private key password before calling this script'
-  exit 0
-fi
-
-#
 # Create crypto keys according to tutorials
 #
+TH_COOKIE_KEY_PASS='Password1'
 openssl ecparam -name prime256v1 -genkey -noout -out private.key
 openssl ec -in private.key -pubout -out public.crt
 openssl pkcs8 -topk8 -in private.key -out private-key.pkcs8 -passout pass:"$TH_COOKIE_KEY_PASS"
